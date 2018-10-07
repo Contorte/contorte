89c9019f43a5c46ba5035e455fb796deb4b22889
山本愛子 <a0i2k0o8@gmail.com>
2018-10-07 16:28:15 +0900

    fix_bug

--- app/src/main/java/com/aihana/android/contorte/MainActivity.kt
+++ app/src/main/java/com/aihana/android/contorte/MainActivity.kt
@@ -4,7 +4,6 @@ package com.aihana.android.contorte
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 
